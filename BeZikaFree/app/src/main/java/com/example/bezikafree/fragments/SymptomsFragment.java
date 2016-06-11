d75b2c2990500a8630fd25118968506f27a09538
adao1123 <adao1193@gmail.com>
2016-06-10 21:30:08 -0700

    Added fragments

---
+++ BeZikaFree/app/src/main/java/com/example/bezikafree/fragments/SymptomsFragment.java
@@ -0,0 +1,27 @@
+package com.example.bezikafree.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bezikafree.R;
+
+/**
+ * Created by adao1 on 6/10/2016.
+ */
+public class SymptomsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.frag_symptoms,container,false);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
